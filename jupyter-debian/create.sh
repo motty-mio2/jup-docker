12d06b8e33ee847ee2f7666b348afffbbc569ae5
motty.mio2 <motty.mio2@gmail.com>
2021-05-31 12:34:06 +0900

    update mount path

--- jupyter-debian/create.sh
+++ jupyter-debian/create.sh
@@ -1,1 +1,3 @@
-docker create -it -e TZ=Asia/Tokyo --name jupdeb39 -p 8888:8888 --mount type=bind,source=${path_to_target},target=/root/jnote jupyter:deb3.9
+#!/bin/bash
+
+docker create -it -e TZ=Asia/Tokyo --name jupdeb39 -p 8888:8888 -u $(id -u $USER) --mount type=bind,source=~/jnote,target=/jnote --mount type=bind,source=/etc/passwd,target=/etc/passwd:ro --mount type=bind,source=/etc/group,target=/etc/group:ro jupyter:deb3.9
--- jupyter-ubuntu-gpu/create.sh
+++ jupyter-ubuntu-gpu/create.sh
@@ -1,1 +1,6 @@
-docker create -it -e TZ=Asia/Tokyo --gpus all --name jupgdeb39 -p 8888:8888 --mount type=bind,source=/mnt/share,target=/root/jnote jupyter:g-deb3.9
+#! /bin/bash
+
+docker create -it -e TZ=Asia/Tokyo --gpus all --name jupgdeb39 -p 8888:8888 --mount type=bind,source=/mnt/share,target=/jnote -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro -u $(id -u $USER):$(id -g $USER) jupyter:g-deb3.9
+
+
+#docker run -it -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro -u $(id -u $USER):$(id -g $USER) ubuntu bash
